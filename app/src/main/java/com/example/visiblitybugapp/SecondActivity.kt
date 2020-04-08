2a18d59955f48aded012a49abb7eb240baed5cd6
Daniel Kim <daniel.seunkim@gmail.com>
2020-04-08 15:53:58 -0700

    More details

--- app/src/main/java/com/example/visiblitybugapp/SecondActivity.kt
+++ app/src/main/java/com/example/visiblitybugapp/SecondActivity.kt
@@ -1,11 +1,14 @@
 package com.example.visiblitybugapp
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.visiblitybugapp.databinding.SecondBinding
+import kotlinx.android.synthetic.main.second.*
 
 class SecondActivity : AppCompatActivity() {
     private val secondVM by viewModels<SecondVM> {
@@ -19,5 +22,11 @@ class SecondActivity : AppCompatActivity() {
         ).apply {
             viewModel = secondVM
         }
+    }
+
+    override fun onStop() {
+        Log.d("SecondActivity", "Is Visible Before onStop: ${progressBar.visibility == View.VISIBLE}")
+        super.onStop()
+        Log.d("SecondActivity", "Is Visible After onStop: ${progressBar.visibility == View.VISIBLE}")
     }
 }
